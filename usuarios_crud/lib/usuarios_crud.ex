fb68774f13f02b3fbee487bef00bd6abaa5ff958
Ana Clara <77753261+anaclrra@users.noreply.github.com>
2024-01-05 12:59:01 +0000

    add: adicionado menu no sistema de gerenciador de usuarios

--- usuarios_crud/lib/usuarios_crud.ex
+++ usuarios_crud/lib/usuarios_crud.ex
@@ -1,4 +1,15 @@
 defmodule UsuariosCrud do
+  @menu """
+  Menu do sistema
+  =============
+  1. Criar
+  2. Listar
+  3. Atualizar
+  4. Excluir
+  5. Sair
+  Entre com sua opção:
+  """
+
   def iniciar_link() do
     Agent.start_link(fn -> [] end, name: __MODULE__)
   end
@@ -31,5 +42,58 @@ defmodule UsuariosCrud do
     Agent.update(__MODULE__, fn usuarios ->
       Enum.reject(usuarios, &(&1.id == id))
     end)
+  end
+
+  def principal() do
+    op = IO.gets(@menu)
+         |> String.trim()
+         |> String.to_integer()
+
+    case op do
+      1 -> criar()
+      2 -> listar()
+      3 -> alterar()
+      4 -> excluir()
+      5 -> IO.puts("Até logo")
+      _ -> IO.puts("Opção inválida")
+    end
+
+    unless op == 5, do: principal()
+  end
+
+  defp criar do
+    IO.puts("Digite o nome do usuário:")
+    nome = IO.gets("") |> String.trim()
+    IO.puts("Digite o email do usuário:")
+    email = IO.gets("") |> String.trim()
+    adicionar_usuario(nome, email)
+    IO.puts("Usuário adicionado com sucesso.")
+  end
+
+  defp listar do
+    IO.puts("Lista de usuários:")
+    listar_usuarios()
+    |> Enum.each(fn %{id: id, nome: nome, email: email} ->
+      IO.puts("ID: #{id}, Nome: #{nome}, Email: #{email}")
+    end)
+  end
+
+  defp alterar do
+    IO.puts("Digite o ID do usuário a ser atualizado:")
+    id = IO.gets("") |> String.trim() |> String.to_integer()
+    IO.puts("Digite o novo nome (deixe em branco para não alterar):")
+    nome = IO.gets("") |> String.trim()
+    IO.puts("Digite o novo email (deixe em branco para não alterar):")
+    email = IO.gets("") |> String.trim()
+    novos_atributos = Enum.filter([nome: nome, email: email], fn {_, v} -> v != "" end) |> Enum.into(%{})
+    atualizar_usuario(id, novos_atributos)
+    IO.puts("Usuário atualizado com sucesso.")
+  end
+
+  defp excluir do
+    IO.puts("Digite o ID do usuário a ser removido:")
+    id = IO.gets("") |> String.trim() |> String.to_integer()
+    remover_usuario(id)
+    IO.puts("Usuário removido com sucesso.")
   end
 end
--- usuarios_crud/lib/usuarios_tarefas.ex
+++ usuarios_crud/lib/usuarios_tarefas.ex
@@ -1,6 +1,17 @@
 defmodule UsuariosTarefas do
   alias UsuariosCrud, as: Crud
 
+  @menu """
+  Menu do sistema
+  =============
+  1. Criar
+  2. Listar
+  3. Atualizar
+  4. Excluir
+  5. Sair
+  Entre com sua opção:
+  """
+
   def adicionar_usuario(nome, email) do
     Task.async(fn -> Crud.adicionar_usuario(nome, email) end)
   end
@@ -15,5 +26,62 @@ defmodule UsuariosTarefas do
 
   def remover_usuario(id) do
     Task.async(fn -> Crud.remover_usuario(id) end)
+  end
+
+  def principal() do
+    op = IO.gets(@menu)
+         |> String.trim()
+         |> String.to_integer()
+
+    case op do
+      1 -> criar()
+      2 -> listar()
+      3 -> alterar()
+      4 -> excluir()
+      5 -> IO.puts("Até logo")
+      _ -> IO.puts("Opção inválida")
+    end
+
+    unless op == 5, do: principal()
+  end
+
+  defp criar do
+    IO.puts("Digite o nome do usuário:")
+    nome = IO.gets("") |> String.trim()
+    IO.puts("Digite o email do usuário:")
+    email = IO.gets("") |> String.trim()
+    adicionar_usuario(nome, email)
+    |> Task.await()
+    IO.puts("Usuário adicionado com sucesso.")
+  end
+
+  defp listar do
+    IO.puts("Lista de usuários:")
+    listar_usuarios()
+    |> Task.await()
+    |> Enum.each(fn %{id: id, nome: nome, email: email} ->
+      IO.puts("ID: #{id}, Nome: #{nome}, Email: #{email}")
+    end)
+  end
+
+  defp alterar do
+    IO.puts("Digite o ID do usuário a ser atualizado:")
+    id = IO.gets("") |> String.trim() |> String.to_integer()
+    IO.puts("Digite o novo nome (deixe em branco para não alterar):")
+    nome = IO.gets("") |> String.trim()
+    IO.puts("Digite o novo email (deixe em branco para não alterar):")
+    email = IO.gets("") |> String.trim()
+    novos_atributos = Enum.filter([nome: nome, email: email], fn {_, v} -> v != "" end) |> Enum.into(%{})
+    atualizar_usuario(id, novos_atributos)
+    |> Task.await()
+    IO.puts("Usuário atualizado com sucesso.")
+  end
+
+  defp excluir do
+    IO.puts("Digite o ID do usuário a ser removido:")
+    id = IO.gets("") |> String.trim() |> String.to_integer()
+    remover_usuario(id)
+    |> Task.await()
+    IO.puts("Usuário removido com sucesso.")
   end
 end
